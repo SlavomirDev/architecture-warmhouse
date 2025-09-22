720909f9a876d99e548a0838bbbfa1cb7aa68f24
SlavomirDev <slavomir.domian@yandex.ru>
2025-09-22 20:03:00 +0200

    Sprint 1

---
+++ apps/temperature-api/Controllers/TemperatureController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using TemperatureApi.Models;
+
+namespace TemperatureApi.Controllers;
+
+[ApiController]
+[Route("temperature")]
+public class TemperatureController : ControllerBase
+{
+    [HttpGet("{id}")]
+    public TemperatureResponse GetTemperature([FromRoute(Name = "id")] string sensorId, [FromQuery(Name = "location")] string? location = null)
+    {
+        return new TemperatureResponse()
+        {
+            Value = Random.Shared.Next(0, 36),
+            Status = "Ok",
+            Location = location ?? "Unknown",
+            SensorId = sensorId,
+            Timestamp = DateTime.Now
+        };
+    }
+}
